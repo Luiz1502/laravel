f18f7392cf2149069e3d339967b307a948c98741
Luiz1502 <luizguluizas@gmail.com>
2025-03-11 14:46:55 -0300

    criar controladores e rotas

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,11 @@ use App\Http\Middleware\LogAcessoMiddleware;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
+Route::get('/principal', [App\Http\Controllers\Principal::class, 'principal']);
+
+Route::get('/dizer-ola', [App\Http\Controllers\Principal::class, 'dizerOla']);
+
+Route::get('/conectar', [App\Http\Controllers\Usuario::class, 'conectar']);
+
+Route::get('/desconectar', [App\Http\Controllers\Usuario::class, 'desconectar']);
 
